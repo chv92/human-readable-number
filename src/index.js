3e087a4e8a2daa6f125d8346e935281931e1bf88
chv92 <vitalii.ch92@gmail.com>
2021-02-27 22:09:25 +0100

    Test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let toTwenty = [zero, one, two, three, four, five, six, seven,
+        eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen,
+        seventeen, eighteen, nineteen,];
+    let tens = [twenty, thirty, forty, fifty, sixty, seventy, eighty, ninety];
+    let nigty = ['nine hundred ninety nine']
+    let output = '';
+    if (number <= 19) {
+        output = toTwenty[number];
+        return output;
+    }
+    if (number.length === 2) {
+        for (let i = 0; i < number.length; i++) {
+            number = number.toString();
+            if (number[i + 1] === 0) {
+                output = tens[number - 2];
+            }
+            else {
+                output += output;
+            }
+        }
+    }
+    if (number.length === 3) {
+        // number = number.toString();
+        for (let i = 0; i < number.length; i++) {
+            if (number[i + 1] === 0 && number[i + 2] === 0) {
+                output = `${tens[number[i] - 2]} + 'hundret'`;
+
+            }
+
+            else {
+                output += nigty[i];
+            }
+        }
+
+        return output;
+
+    }
+
+
+
+
+
 }
